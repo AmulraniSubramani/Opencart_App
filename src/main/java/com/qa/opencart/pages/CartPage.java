e4db7cb500c67cfa1017494cd4b3dd59fb14c7db
unknown <amulsubramani@gmail.com>
2021-10-17 16:05:20 +0530

    adding new cart page in to cart

---
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class CartPage {
+
+	public CartPage() {
+		System.out.println("this is cart page");
+	}
+	
+	public void addToCart() {
+		System.out.println("Add to cart");
+	}
+}
