2c8a70eef7405cf2d94858259af517c825e76075
unknown <amulsubramani@gmail.com>
2021-10-20 21:58:46 +0530

    Adding code after stash

--- src/main/java/com/qa/opencart/pages/AmulPage.java
+++ src/main/java/com/qa/opencart/pages/AmulPage.java
@@ -5,7 +5,11 @@ public class AmulPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Demo1");
+<<<<<<< Updated upstream
 		System.out.println("Demo2 by tom");
+=======
+		System.out.println("Demo 3 by amul");
+>>>>>>> Stashed changes
 	}
 
 }
