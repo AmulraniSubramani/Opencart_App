9f6a83ebc6732822c303da98d0331e39c1f65c45
AmulraniSubramani <92517265+AmulraniSubramani@users.noreply.github.com>
2021-10-20 21:49:03 +0530

    Update AmulPage.java

--- src/main/java/com/qa/opencart/pages/AmulPage.java
+++ src/main/java/com/qa/opencart/pages/AmulPage.java
@@ -5,6 +5,7 @@ public class AmulPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Demo1");
+		System.out.println("Demo2 by tom");
 	}
 
 }
